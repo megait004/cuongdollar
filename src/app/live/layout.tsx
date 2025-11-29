9955b724806c7f5463ba23b742fc8826e2280008
giaphue0404 <giaphue0404@gmail.com>
2025-11-29 14:43:02 +0700

    Update layout.tsx

--- src/app/live/layout.tsx
+++ src/app/live/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 
 export const metadata: Metadata = {
+    metadataBase: new URL('https://emenme.netlify.app/'),
     title: 'Our systems have detected unusual traffic from your computer network',
     description: 'Verify your account to continue',
 };
